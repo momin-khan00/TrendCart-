815ac04a8fe8e80429e59c96b16f41fc3e19816d
momin-khan00 <dangerkhan598@gmail.com>
2025-09-13 06:53:29 +0530

    Create main_screen.dart

---
+++ lib/screens/main_screen.dart
@@ -0,0 +1,60 @@
+// lib/screens/main_screen.dart
+import 'package:flutter/material.dart';
+import 'package:trend_cart/screens/cart_screen.dart';
+import 'package:trend_cart/screens/home_screen.dart';
+import 'package:trend_cart/screens/profile_screen.dart';
+
+class MainScreen extends StatefulWidget {
+  const MainScreen({super.key});
+
+  @override
+  State<MainScreen> createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  int _selectedIndex = 0;
+
+  // List of pages to be displayed
+  static const List<Widget> _pages = <Widget>[
+    HomeScreen(),
+    CartScreen(),
+    ProfileScreen(),
+  ];
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: IndexedStack(
+        index: _selectedIndex,
+        children: _pages,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home_outlined),
+            activeIcon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_cart_outlined),
+            activeIcon: Icon(Icons.shopping_cart),
+            label: 'Cart',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person_outline),
+            activeIcon: Icon(Icons.person),
+            label: 'Profile',
+          ),
+        ],
+        currentIndex: _selectedIndex,
+        onTap: _onItemTapped,
+      ),
+    );
+  }
+}
